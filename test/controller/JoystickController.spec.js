5ab29ddd0c8eac57872d38df79e84f8d202ce324
Nadav Dav <nadavik@gmail.com>
2013-04-13 20:17:42 +0300

    added joystickController

--- test/controller/JoystickController.spec.js
+++ test/controller/JoystickController.spec.js
@@ -4,7 +4,13 @@ var rek = require('rekuire');
 var EventEmitter = rek('events').EventEmitter;
 var JoystickController = rek('JoystickController');
 
-xdescribe("JoystickController", function(){
+var MODE_UP = 1;
+var MODE_RIGHT = 2;
+var MODE_DOWN = 3;
+var MODE_LEFT = 4;
+var NO_MODE = -1;
+
+describe("JoystickController", function(){
     var device = new EventEmitter();
     var X_INPUT_INDEX = 0;
     var Y_INPUT_INDEX = 1;
@@ -16,12 +22,23 @@ xdescribe("JoystickController", function(){
         it("should set mode according to the joystick position", function(){
             var callback = createSpy('callback');
             JoystickController(device, X_INPUT_INDEX, Y_INPUT_INDEX, RESOLUTION,callback);
-            emitStream(device,[[0,0],[0,0],[127,255],[127,255]]);
-            expect(callback).toHaveBeenCalledWith(1,[0]);
+            emitStream(device,[[127,127],[127,127],[127,255]]);
+            expect(callback).toHaveBeenCalledWith(MODE_UP,0);
+        });
+
+        // for example, when the joystick is pressed up, it should engage mode 1
+        it("should not switch between mode if not returned to center", function(){
+            var dataReceived = [];
+            var callback = createSpy('callback').andCallFake(function(mode, value){
+                dataReceived.push([mode, value]);
+            });
+            JoystickController(device, X_INPUT_INDEX, Y_INPUT_INDEX, RESOLUTION,callback);
+            emitStream(device,[[127,127],[127,127],[127,255],[255,127]]);
+            expect(dataReceived).toEqual([ [MODE_UP, 0], [MODE_UP, 64]]);
         });
 
 
-        it("should set mode according to the joystick position", function(){
+        xit("should set mode according to the joystick position", function(){
             var dataReceived = [];
             var callback = createSpy('callback').andCallFake(function(data){
                 dataReceived.push(data);
@@ -35,6 +52,6 @@ xdescribe("JoystickController", function(){
 
 function emitStream(device,stream){
     for( var i in stream){
-        device.emit('data',[stream[i[0]], stream[i[1]]]);
+        device.emit('data',stream[i]);
     }
 }
